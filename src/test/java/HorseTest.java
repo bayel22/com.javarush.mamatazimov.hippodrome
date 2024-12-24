0871a56a084be28b317fbfe1c86fad3f61b60e85
by L <bayel.mamatazimov22@gmail.com>
2024-12-24 19:30:44 +0300

    add junit-jupiter-params dependency add HorseTest (constructor tests)

---
+++ src/test/java/HorseTest.java
@@ -0,0 +1,62 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HorseTest {
+
+    private Horse horse;
+
+    @Test
+    void throwsExceptionWithMessageWhenNameIsNull() {
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new Horse(null, 1, 1));
+        assertEquals("Name cannot be null.", illegalArgumentException.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "\t", "\n", "\t\n", "\r"})
+    void throwsExceptionWhenNameIsNullOrWhitespaceSymbols(String message) {
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new Horse(message, -1, -1));
+        assertEquals("Name cannot be blank.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void throwsExceptionWhenSpeedIsNegative() {
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new Horse("horse", -1, 1));
+        assertEquals("Speed cannot be negative.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void throwsExceptionWhenDistanceIsNegative() {
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new Horse("horse", 1, -1));
+        assertEquals("Distance cannot be negative.", illegalArgumentException.getMessage());
+    }
+
+    @Test
+    void getName() {
+        horse = new Horse("Horse", 1,1);
+        assertEquals("Horse", horse.getName());
+    }
+
+    @Test
+    void getSpeed() {
+        horse = new Horse("Horse", 20,1);
+        assertEquals(20, horse.getSpeed());
+    }
+
+    @Test
+    void getDistance() {
+        horse = new Horse("Horse", 1,1);
+        assertEquals(1, horse.getDistance());
+    }
+
+    @Test
+    void move() {
+
+    }
+
+    @Test
+    void getRandomDouble() {
+    }
+}
